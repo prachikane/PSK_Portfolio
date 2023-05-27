3b54b1add1a7aa798b1829485caca8aad4c56036
Prachi Kane <prachikane27@gmail.com>
2023-05-27 13:09:20 -0500

    main page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout/>}/>
+        <Route path="/PSK_Portfolio" element={<Layout/>}/>
       </Routes>
     </>
   );
